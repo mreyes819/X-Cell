a5ce7026b622132396aa21e861096017542eef81
mttrys <mttrys@gmail.com>
2017-04-26 10:34:56 -0700

    35 - Render data rows. 37 - Select the current cell

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -11,6 +11,53 @@ describe('table-view', () => {
     document.documentElement.innerHTML = html;
   });
 
+  describe('table body', () => {
+
+    it('highlights the current cell when clicked', () => {
+      // set up the initial state
+      const model = new TableModel(10,5);
+      const view = new TableView(model);
+      view.init()
+
+      // inspect the initial state
+      let trs = document.querySelectorAll('TBODY TR');
+      let td = trs[2].cells[3];
+      expect(td.className).toBe('');
+
+      // simulate user action
+      td.click();
+
+      // inspect the resulting state
+      trs = document.querySelectorAll('TBODY TR');
+      td = trs[2].cells[3];
+      expect(td.className).not.toBe('');
+    });
+    it('has the right size', () => {
+      // set up the inital state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
+    });
+
+    it('fills in the values from the model', () => {
+      // set up the inital state
+      const model = new TableModel(3,3);
+      const view = new TableView(model);
+      model.setValue({col:2, row:1}, '123');
+      view.init();
+
+      // inspect the intial state
+      const trs = document.querySelectorAll('TBODY TR');
+      expect(trs[1].cells[2].textContent).toBe('123');
+    });
+  });
+
   describe('table header', () => {
     it('has valid column header labels', () => {
       // set up the inital state
