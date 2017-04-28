a9b78db932ff8310122e67c9c414ec227633e14f
mttrys <mttrys@gmail.com>
2017-04-28 10:02:10 -0700

    ignore public/js/bundle.js added to .gitignore

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -70,6 +70,27 @@ class TableModel {
   setValue(location, value) {
     this.data[this._getCellId(location)] = value;
   }
+
+  getColumn(column) {
+    let values = {};
+    for (let r = 0; r < this.numRows; r++){
+      const location = {col: column, row: r};
+      const value = this.data[this._getCellId(location)];
+      values[r] = value;
+    }
+    return values
+  }
+
+  getRow(row){
+    let values = {};
+    for(let c = 0; c < this.numCols; c++){
+      const location = {col:c, row: row};
+      const value = this.data[this._getCellId(location)];
+      values[r] = value;
+    }
+    return values
+  }
+
 }
 
 module.exports = TableModel;
